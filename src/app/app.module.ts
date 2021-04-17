d6b0f0515cd1fcfdeddb56ef6cf98bcfa83d8b14
AB <AB2k2@users.noreply.github.com>
2021-04-17 15:28:21 +0530

    FE: Landing Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { HeroBodyComponent } from './components/body/hero-body/hero-body.component';
+import { HeroFooterComponent } from './components/body/hero-footer/hero-footer.component';
+import { SectionComponent } from './components/body/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    HeroBodyComponent,
+    HeroFooterComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/body/hero-footer/hero-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-footer',
+  templateUrl: './hero-footer.component.html',
+  styleUrls: ['./hero-footer.component.css']
+})
+export class HeroFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
