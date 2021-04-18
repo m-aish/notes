c666e99fdd908636570917b1f9744b7bed1c525a
Muthu Aiswaryaa Swaminathan <withlove.aish@gmail.com>
2021-04-18 12:14:47 +0530

    Questions Service & removed /questions

--- src/app/components/body/cards-container/panels/card-preview/card-preview.component.ts
+++ src/app/components/body/cards-container/panels/card-preview/card-preview.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {QuestionsService} from '../../../../../services/questions.service';
 @Component({
   selector: 'app-card-preview',
   templateUrl: './card-preview.component.html',
   styleUrls: ['./card-preview.component.css']
 })
 export class CardPreviewComponent implements OnInit {
+  questions: any;
 
-  constructor() { }
+  constructor(private questionsService: QuestionsService) { }
 
   ngOnInit(): void {
   }
-
+  getQuestions(){
+    this.questions = this.questionsService.getQuestions("The Vision API client libraries accesses the global API endpoint (vision.googleapis.com) by default. To store and process your data in the European Union only, you need to explicitly set the endpoint (eu-vision.googleapis.com). The code samples below show how to configure this setting.");
+    if (this.questions)
+    console.log(this.questions[0])
+    //console.log(this.questions);
+  }
 }
--- src/server/appserver.ts
+++ src/server/appserver.ts
@@ -1,16 +1,6 @@
 const Comprehender = require("./comprehender.ts");
 const axios = require('axios');
 let comprehender = new Comprehender();
-// comprehender.comprehendText("Jeff Bezos says hi").then(response => {
-//         Output = response.result
-//         //return Output
-//         console.log(JSON.stringify(response.result, null, 2));
-//     })
-//     .catch(err => {
-//         Output = err
-//         //return Output;
-//         console.log('error: ', err);
-//     });
 const express = require('express');
 const cors = require('cors');
 var corsOptions = {
@@ -35,24 +25,6 @@ app.get('/keywords', cors(corsOptions), (req, res) => {
             res.send(err);
         });
 
-});
-app.get('/questions', cors(corsOptions), (req, res)=>{
-    app.use(bodyParser.text({ type: 'text/html' }));
-    let text = req.body.text;
-    axios.post('https://gs4ossx7yj.execute-api.us-east-1.amazonaws.com/dev/text', { "text": text },{headers:{'Content-Type':'text/plain'}})
-    .then(response => {
-        let qnapair = response.data.map(item => {
-            const container ={};
-            container['QuestionPrompt'] = item.QuestionPrompt;
-            container['CorrectAnswer'] = item.CorrectAnswer;
-            return container;
-        })
-        res.send(qnapair);
-    })
-    .catch(error => {
-        output = error;
-        res.send(error)
-    });
 });
 
 app.listen(port, () => {
